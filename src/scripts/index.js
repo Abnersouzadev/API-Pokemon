b9923a3f71284bcb199145fe6494660027180422
Abner <contato.abnersouza@gmail.com>
2023-01-09 12:12:05 -0300

    3

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,51 @@
-/* Monte sua lógica aqui */
+async function renderizaPokemons() {
+    const ulTag = document.querySelector("ul");
+    const tagLoading = document.querySelector("#loading");
+    const listaDePokemons = await PokeAPI();
+  
+    setTimeout(() => {
+      listaDePokemons.results.forEach((pokemon) => {
+          tagLoading.innerHTML = "";
+          const numeroNaPokedex = pokemon.url.slice(34, -1);
+  
+        ulTag.insertAdjacentHTML(
+          "beforeend",
+          `
+                <li>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name}>
+                <h3>${pokemon.name}</h3>
+                </li>
+          `
+        );
+      });
+    }, 1500);
+  }
+  renderizaPokemons();
+
+  async function pesquisa(){
+    let ul = document.querySelector("ul");
+
+    let input = document.querySelector("input")
+
+    let pesquisaPokemon = await pesquisaPoke(input.value);
+
+    let numeroPokedex = pesquisaPokemon.id;
+
+    ul.innerHTML = "";
+
+    ul.insertAdjacentHTML('beforeend', `
+    <li class="flex_center">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroPokedex}.png" alt=${pesquisaPokemon.name}>
+        <h3 class="poke_name">${pesquisaPokemon.name}</h3>
+    </li>`)
+
+  }
+
+  let botao = document.querySelector("button")
+
+  botao.addEventListener("click", async (event) => {
+    await pesquisa();
+    event.preventDefault
+  })
+  
+
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,9 @@
-/* Monte a lógica das requisições aqui */
+async function PokeAPI() {
+    const pokemonDaAPI = await fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((resp) => resp.json())
+      .catch((error) => console.log(error));
+  
+    return pokemonDaAPI;
+  }
+  
+  PokeAPI()
